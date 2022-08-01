f1a4eafd78c009b14c17ede26b85c10b136851f5
Joe Pea <joe.pea@nasa.gov>
2022-08-01 13:33:05 -0700

    add code formatting configuration

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'all',
+	bracketSpacing: false,
+	printWidth: 120,
+	arrowParens: 'avoid',
+
+	// Note that the sibling .editorconfig file provides further overrides that
+	// Prettier honors (uses as overrides).
+}
