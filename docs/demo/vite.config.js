8ac934950465cf2bf29f0efb85f737754ba0676d
Milan Hauth <milahu@gmail.com>
2022-09-20 12:35:22 +0200

    vite config: produce relative paths in html

--- docs/demo/vite.config.js
+++ docs/demo/vite.config.js
@@ -2,6 +2,7 @@ import {defineConfig} from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 
 export default defineConfig({
+	base: './', // relative paths in html
 	plugins: [solidPlugin()],
 	build: {
 		target: 'esnext',
